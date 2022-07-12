8c9c587c5016ec8c3e63052690b6978c2a427dec
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-07-12 13:21:51 +0100

    Hierarchy Tweak + Celestial PF Tweak
    - Added code to InitialiseVelocity.cs to shift Celestials in the Hierarchy to be siblings of each other under the "System" object. This should fix the issue where grappling the Sun meant you picked up the entire Solar System, this should no longer happen as Celestials are orphaned after initial orbital velocities are applied
    
    - Tweaked Celestial PFs to use the collider attached to the main (empty) gameobject's collider in the XR Grab Interactable script. This means that you should always pick up the object that has the RigidBody and other vital components

--- VR Solar Sys Simulator/Assets/Scripts/Simulation/InitialiseVelocity.cs
+++ VR Solar Sys Simulator/Assets/Scripts/Simulation/InitialiseVelocity.cs
@@ -6,25 +6,20 @@ public class InitialiseVelocity : MonoBehaviour
 {
     public GameObject[] celestialList;
     SimulationSettings simSettings;
+    public bool findSystemObj;
+    public GameObject systemObj;
 
     // Start is called before the first frame update
     void Start()
     {
         simSettings = gameObject.GetComponent<SimulationSettings>();
-        //rigidBody = GetComponent<Rigidbody>();
-        //celestialProps = GetComponent<CelestialProperties>();
-        //if (hasOrbitingBodies)
-        //{
-        //    MainBody = gameObject;
-        //    foreach (GameObject OrbitingBody in OrbitingBodies)
-        //    {
-        //        OrbitingBody.GetComponent<CelestialProperties>().hostObj = MainBody.gameObject;
-        //    }
-        //    InitialVelocity();
-        //}
+        celestialList = simSettings.celestials;
+        if (findSystemObj)
+        {
+            systemObj = gameObject;
+        }
 
         InitialVelocity();
-
     }
 
     private void OnValidate()
@@ -70,49 +65,13 @@ public class InitialiseVelocity : MonoBehaviour
 
                     Debug.Log("Distance is " + distance + " || " + "SemiMajor is " + semiMajor + " || " + "Velocity of " + child + " is " + child.GetComponent<Rigidbody>().velocity.magnitude + " || " + "Mass of Parent = " + mass1 + " Mass of Child = " + mass2);
                 }
-
             }
 
         }
+        foreach (GameObject celestial in celestialList)
+        {
+            celestial.transform.SetParent(systemObj.transform, true);
+        }
     }
-
-
-    //void InitialVelocity()
-    //{
-    //    foreach (GameObject OrbitingBody in MainCelestials)
-    //    {
-    //        float mass1 = MainBody.GetComponent<Rigidbody>().mass;
-    //        float mass2 = OrbitingBody.GetComponent<Rigidbody>().mass;
-    //        float distance = Vector3.Distance(MainBody.transform.position, OrbitingBody.transform.position);
-    //        float semiMajor = OrbitingBody.GetComponent<CelestialProperties>().semiMajor;
-    //        Vector3 velocityDirection = OrbitingBody.GetComponent<CelestialProperties>().initDirection; // Defines temporary vector direction for velocity at periapsis from CelestialProperties.cs
-
-    //        OrbitingBody.GetComponent<CelestialProperties>().orbitalPeriod = Mathf.Sqrt(4 * Mathf.Pow(Mathf.PI, 2) * Mathf.Pow((semiMajor), 3f) / (simSettings.gravitationalConstant * (mass2 + mass1))); // Using K3L
-
-    //        OrbitingBody.GetComponent<Rigidbody>().velocity += velocityDirection * Mathf.Sqrt(simSettings.gravitationalConstant * (mass1 + mass2) * ((2 / distance) - (1 / semiMajor))) + MainBody.GetComponent<Rigidbody>().velocity; // Gives celestial calculated velocity plus current velocity of host celestial so satellite moves correctly relative to host
-
-    //        Debug.Log("Distance is " + distance + " || " + "SemiMajor is " + semiMajor + " || " + "Mass of " + MainBody + " = " + mass1 + " || " + "Mass of " + OrbitingBody + " = " + mass2 + " || \n" + "Velocity applied to " + OrbitingBody.name + " is " + OrbitingBody.GetComponent<Rigidbody>().velocity.magnitude);
-
-    //        if (OrbitingBody.GetComponent<CelestialProperties>().hasOrbitingBodies)
-    //        {
-    //            Satellites = OrbitingBody.GetComponent<CelestialProperties>().orbitingBodies;
-
-    //            foreach (GameObject Satellite in Satellites)
-    //            {
-    //                float m1 = OrbitingBody.GetComponent<Rigidbody>().mass;
-    //                float m2 = Satellite.GetComponent<Rigidbody>().mass;
-    //                float r = Vector3.Distance(OrbitingBody.transform.position, Satellite.transform.position);
-    //                float a = Satellite.GetComponent<CelestialProperties>().semiMajor;
-    //                Vector3 vel_Direction = Satellite.GetComponent<CelestialProperties>().initDirection; // Defines temporary vector direction for velocity at periapsis from CelestialProperties.cs
-
-    //                Satellite.GetComponent<CelestialProperties>().orbitalPeriod = Mathf.Sqrt(4 * Mathf.Pow(Mathf.PI, 2) * Mathf.Pow((a), 3f) / (simSettings.gravitationalConstant * (m1 + m2))); // Using K3L
-
-    //                Satellite.GetComponent<Rigidbody>().velocity += velocityDirection * Mathf.Sqrt(simSettings.gravitationalConstant * (m1 + m2) * ((2 / r) - (1 / a))) + OrbitingBody.GetComponent<Rigidbody>().velocity; // Gives celestial calculated velocity plus current velocity of host celestial so satellite moves correctly relative to host
-
-    //                Debug.Log("Distance is " + r + " || " + "SemiMajor is " + a + " || " + "Mass of " + OrbitingBody + " = " + m1 + " || " + "Mass of " + Satellite + " = " + m2 + " || \n" + "Velocity applied to " + Satellite.name + " is " + Satellite.GetComponent<Rigidbody>().velocity.magnitude);
-    //            }
-    //        }
-    //    }
-    //}
 }
 
